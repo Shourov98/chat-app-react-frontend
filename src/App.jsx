53e7df85a29843da36091d58131f0b7846df4a6c
Shourov98 <mdmahbub0412@gmail.com>
2025-05-24 19:47:22 +0600

    Tailwind configure and installation

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+//import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h2 className='bg-gray-800 text-white py-5 px-3.5'>Hello Tailwind 4</h2>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
